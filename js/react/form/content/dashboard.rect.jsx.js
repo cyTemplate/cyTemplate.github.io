1f988ba84d0f7b4a96d4b105c71f3abc70ee334b
Han <hunterhan@me.com>
2016-02-18 17:51:32 +1100

    Update dashboard.rect.jsx.js

--- js/react/form/content/dashboard.rect.jsx.js
+++ js/react/form/content/dashboard.rect.jsx.js
@@ -1,42 +1,77 @@
-/***************************
- * React dashboard class.
- * Yu Han 2016
- * *************************/
+/*******************************
+ * React TwoPartModelView v 0.1
+ * app > header + ( leftblock + rightblock )
+ * Yu Han 2016 @claydata.com
+ * hany@mynsmg.com.au
+ * *****************************/
 
-//Webpage
+//App 
+//set 1st as default selected component (display on the right-hand side) passed from model
 var App=React.createClass({
-   componentDidMount:function(){
-       console.log('Render App.Generate id:'+this.id);
-   } ,
-    
+	
+	getInitialState: function() {
+		var state={modeID:'',components:this.props.model};
+		if(typeof this.props.model.map =='undefined')
+	   {
+		   //map JSON into JS array 
+			state.components= Object.keys(this.props.model).map(function(k) { return {k:this.props.model[k]}});
+	   };
+	   state.modeID=0;
+	   console.log('App State: ',state);
+		return state;
+	  },
+	handleComponentChange:function(i){
+		 //change modeID
+		 console.log('Selected id:',i);
+		
+		this.setState({modeID:i});
+	},
+   
    render:function(){
+	    console.log('Render App. Left Menu id:'+this.id);
        return(
-       <div className="df-col-flex-">
-            <div className="df-col-sm3-silver-">
-                
-                <LeftBlock leftMenuOptionList={this.props.leftMenuOptionList} id='leftMenu'/>
-            </div>
-            <div className="df-col-sm7-silver-">
-               
-                <RightBlock ableToggleLetMenu='true' toggleMenuID='leftMenu'/>
-            </div>
+	   <div className="container df-flex-jfmid-">
+       <div className="df-col-sm9-flex-ver-start-">
+			<AppHeader ableToggleLetMenu='true' toggleMenuID='leftMenu'/>
+			<div className="df-flex-jfsb-">
+				<div className="df-col-sm3-">
+					
+					<LeftBlock components={this.state.components} id='leftMenu' componentChange={this.handleComponentChange}/>
+				</div>
+				<div className="df-col-sm7-">
+					<RightBlock displayConent={this.state.components[this.state.modeID]}/>
+				</div>
+			</div>
        </div>
+	   </div>
     )
    },
 });
-       
+
+var AppHeader=React.createClass({
+	
+	render:function(){
+		 var leftMenuToogleButton=!this.props.ableToggleLetMenu?
+           '' :
+           <div className="navbar-toggle collapsed df-sp-clickable-" data-toggle="collapse" data-target={"#"+this.props.toggleMenuID} aria-expanded="false">
+                    <i className="fa fa-bars df-clickable-"/>
+                </div>;
+				console.log('Render AppHeader. Has toggle menu button:'+this.props.ableToggleLetMenu);
+       return(
+	 <h3 className="df-title-pad-"> Edit Acount {leftMenuToogleButton} <small> Change profile or personal information</small>   </h3>
+	 )
+	}
+	
+});
+
 //LeftBlock
 var LeftBlock=React.createClass({
    
-    componentDidMount:function(){
-      
-        console.log('Render LeftBlock.');
-   } ,
     render:function(){
-       
+        console.log('Render LeftBlock.');
         return(
-            <div className="navbar-collapse collapse df-col-silver- " id={this.props.id}>
-                <VerticalMenu leftMenuOptionList={this.props.leftMenuOptionList} />
+            <div className="navbar-collapse collapse df-col- " id={this.props.id}>
+                <VerticalMenu components={this.props.components}  componentChange={this.props.componentChange}/>
             </div>
     )
     },
@@ -44,18 +79,24 @@ var LeftBlock=React.createClass({
        
 //RightBlock
 var RightBlock=React.createClass({
-    componentDidMount:function(){
-       console.log('Render RightBlock. Has toggle menu button:'+this.props.ableToggleLetMenu);
-   } ,
-   render:function(){
-       var leftMenuToogleButton=!this.props.ableToggleLetMenu?
-           '' :
-           <div className="navbar-toggle collapsed df-sp-clickable-" data-toggle="collapse" data-target={"#"+this.props.toggleMenuID} aria-expanded="false">
-                    <i className="fa fa-bars"/>
-                </div>;
+	//Only render the selected component
+	
+   render:function(){ 
+		//create element based on tag type
+		var displayTagElement='';
+		console.log(this.props.displayConent.content.tag);
+		switch (this.props.displayConent.content.tag)
+		{
+			case 'form':
+				displayTagElement=this.props.displayConent.content.submitButtonText
+			break;
+			default:
+				displayTagElement='No Content to Display.';
+		}
+	console.log('Render RightBlock.',displayTagElement);
        return(
-            <div className="df-col-silver-">
-           {leftMenuToogleButton}
+            <div className="df-col-">
+				{displayTagElement}
             </div>
     )
    },
@@ -64,33 +105,46 @@ var RightBlock=React.createClass({
 
 //VerticalMenu
 var VerticalMenu=React.createClass({
-     componentDidMount:function(){
-       console.log('Render VerticalMenu.');
-     },
-   render:function(){
-     
-       var optionlists=this.props.leftMenuOptionList.map(function(option){
-           
-           return (
-                <li className="df-col-skyblue-sp-clickable--hvrmore-">
-                <a href="#" className="df-mrgl-xsbf-">
-                    {option.name}
-                </a>
-                </li>
-           )
-       });
+	getInitialState:function(){
+		return {'selectedID':0}; 
+	},	
+	handleOnChange:function(i){
+		this.props.componentChange(i);
+		this.setState({selectedID:i});
+		console.log(i);
+	},
+	render:function(){
+		console.log('Render VerficalMenu');
        return(
-            <ul className="nav navbar-nav df-col-silver-">
-            {optionlists}
+            <ul className="nav navbar-nav df-col-">
+            {
+				this.props.components.map(function(component, i) {
+				  return (
+				  <li className="df-col-hvrmore-" onClick={this.handleOnChange.bind(this,i)} key={i}>
+					<ComponentOption componentKey={i} selectedID={this.state.selectedID} componentName={component.option.name}/>
+                </li>
+					 
+				  );
+				}, this)
+		}
             </ul>
         )
    },
 })
 
-$(document).ready(function(){
-    ReactDOM.render(
-      <App leftMenuOptionList={sample.leftMenuOption} />,
-      document.getElementById('app')
-    );
-});
+//Component Option
+var ComponentOption=React.createClass({
+	
+	render:function(){
+		
+		var active=this.props.componentKey==this.props.selectedID?'blue-title-sm-':'skyblue-';
+		console.log('Render Option');
+		return (
+			<a href="#" className={"df-mrgl-xsbf-"+active}>
+						{this.props.componentName}
+			</a>
+		)
+		
+	}
 
+});
